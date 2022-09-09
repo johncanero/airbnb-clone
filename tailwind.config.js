fe96cdf931b81c81e2e5e54f697299e12bf3be76
John Canero <johncanero.official@gmail.com>
2022-09-09 11:21:19 +0800

    Modify Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      pinkRauschAir: '#ff5a5f',
+      cyanAir: '00A699',
+      orangeAir: 'FC642D',
+      darkGrayAir: '484848',
+      darkerGrayAir: '767676',
+    },
+
+    fontFamily: {
+      Roboto: ['Roboto', 'sans-serif'],
+    },
   },
   plugins: [],
 }
