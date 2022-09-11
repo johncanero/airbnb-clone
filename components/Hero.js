9a709608e09191b0434ddb4c8d6e8fcd895ee43f
John Canero <johncanero.official@gmail.com>
2022-09-11 11:52:26 +0800

    Card Styling, Images, and Component

--- components/Hero.js
+++ components/Hero.js
@@ -21,7 +21,7 @@ const Hero = () => {
 
         {/* Hero Content */}
         <div className='mt-10 px-8 md:px-12 lg:px-36 lg:mt-16'>
-            <h1 className='font-bold text-4xl text-center md:text-5xl md:text-left font-Poppins'>Online Experiences</h1>
+            <h1 className='font-bold text-5xl text-center md:text-5xl md:text-left font-Poppins'>Online Experiences</h1>
             <p className='font-light text-center mt-5 md:text-left md:text-2xl md:pr-64 font-Poppins '>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
         </div>
     
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import Card from '../components/Card'
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
 
       <main>
           <Hero />
+          <Card />
       </main>
 
       <footer>
