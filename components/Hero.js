e5fcf270dea501f012cf7345ee3784524a2d49aa
John Canero <johncanero.official@gmail.com>
2022-09-14 23:48:31 +0800

    Airbnb Grid Location Path Image

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import AirbnnbGrid from '../components/images/airbnbGrid.png'
+import AirbnnbGrid from '../public/images/airbnbGrid.png'
 
 const Hero = () => {
   return (
