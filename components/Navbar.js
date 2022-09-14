3f21482de2a49df989efed517b0e8c58c5a94a42
John Canero <johncanero.official@gmail.com>
2022-09-14 23:49:42 +0800

    File Path Image

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import AirbnbLogo from '../components/images/airbnbLogo.png'
+import AirbnbLogo from '../public/images/airbnbLogo.png'
 
 const Navbar = () => {
   return (
