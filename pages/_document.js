17b57868cafcc0466370a86ccd5862551d035a59
John Canero <johncanero.official@gmail.com>
2022-09-16 16:10:04 +0800

    Footer Font Awesome Icons and Styling

--- pages/_document.js
+++ pages/_document.js
@@ -22,8 +22,8 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-            {/* Google Fonts - Custom Document */}
         <Head>
+            {/* Google Fonts - Custom Document */}
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;1,600&display=swap" rel="stylesheet" />
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import Footer from '../components/Footer'
 
 
 
-
 export default function Home() {
   return (
     <div>
