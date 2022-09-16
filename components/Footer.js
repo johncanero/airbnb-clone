aeb1a973bb54190a62a6b12877a953532568a0eb
John Canero <johncanero.official@gmail.com>
2022-09-16 23:35:15 +0800

    Tablet Styling

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ import { faDribbble } from "@fortawesome/free-brands-svg-icons";
 const Footer = () => {
   return (
     <div>
-      <footer className="p-10 sm:p-6 bg-darkGrayAir">
+      <footer className="p-10 sm:p-6 bg-darkGrayAir md:p-12">
         {/* Footer Upper Section */}
         <div className="md:flex md:justify-between">
           {/* Logo */}
@@ -23,7 +23,7 @@ const Footer = () => {
               <a
                 target="_blank"
                 rel="noopener noreferrer"
-                className="w-24 flex items-center"
+                className="w-24 flex items-center md:w-28"
               >
                 <Image src={AirbnbLogo} alt="" />
               </a>
@@ -31,7 +31,7 @@ const Footer = () => {
           </div>
 
           {/* Resources */}
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 md:grid-cols-2 md:gap-20">
             <div>
               <h2 className="mb-4 text-sm font-semibold text-gray-900 uppercase dark:text-white">
                 Resources
@@ -101,7 +101,8 @@ const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-500 lg:my-8" />
 
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+
+          <span className="text-sm text-white sm:text-center">
             © 2022{" "}
             <a href="https://flowbite.com/" className="hover:underline">
               Flowbite™
@@ -109,7 +110,8 @@ const Footer = () => {
             . All Rights Reserved.
           </span>
 
-          <div className="flex mt-6 space-x-6 sm:justify-center sm:mt-0">
+          {/* Icons */}
+          <div className="flex mt-6 space-x-6 sm:justify-center sm:mt-0 md:gap-4">
             <Link href="https://www.facebook.com/canerojohn/">
               <a
                 target="_blank"
