f104b1f1f056fb9411fe20699c43c77b8731014a
John Canero <johncanero.official@gmail.com>
2022-11-05 14:01:46 +0800

    Add Toggle Mode Feature for Dark Mode

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import AirbnbLogo from '../public/images/airbnbLogo.png'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import AirbnbLogo from "../public/images/airbnbLogo.png";
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
-    <div>
-        <nav className='px-8 p-6 bg-white border-gray-200 dark:border-gray-700 shadow-md md:px-10'>
-            <div className='container flex flex-wrap justify-between items-center mx-auto'>
-                <Link href='https://www.airbnb.com/'>
-                    <a target="_blank" rel="noopener noreferrer"x className='w-32 flex items-center'>
-                        <Image 
-                          src={AirbnbLogo}
-                          alt='airbnbLogo.png'
-                          className=''
-                        />
-                        <span></span>
-                    </a>
-                </Link>
-            </div>
-        </nav>
-    </div>
-  )
-}
+    <nav className={props.darkMode ? "dark" : ""}>
+      <div className="px-8 p-6 bg-white border-gray-200 dark:border-gray-700 shadow-md md:px-10">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <Link href="https://www.airbnb.com/">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              x
+              className="w-32 flex items-center"
+            >
+              <Image src={AirbnbLogo} alt="airbnbLogo.png" className="" />
+              <span></span>
+            </a>
+          </Link>
 
-export default Navbar
+          {/* Toggle */}
+          <div className="toggler">
+            <p className="toggler--light">Light</p>
+            <div className="toggler--slider" onClick={props.toggleDarkMode}>
+              <div className="toggler--slider--circle"></div>
+            </div>
+            <p className="toggler--dark">Dark</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,45 @@
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import Card from '../components/Card'
-import Footer from '../components/Footer'
-
-
+import React, { useState } from 'react';
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
   return (
     <div>
       <Head>
         <title>AirBnb Clone</title>
         <meta name="description" content="AirBnb Clone" />
         <meta itemprop="name" content="AirBnb" />
-        <meta itemprop="description" content="AirBnb Clone created with Next JS and Tailwind CSS" />
+        <meta
+          itemprop="description"
+          content="AirBnb Clone created with Next JS and Tailwind CSS"
+        />
         <meta itemprop="image" content="AirBnb Props" />
-        <link rel="icon" href="/airbnbFavicon.png" />     
+        <link rel="icon" href="/airbnbFavicon.png" />
       </Head>
-      
+
       <nav>
-         <Navbar />
+        <Navbar 
+          darkMode={darkMode} 
+          toggleDarkMode={toggleDarkMode} 
+        />
       </nav>
 
       <main>
-          <Hero />
-          <Card />
+        <Hero />
+        <Card />
       </main>
 
       <footer>
-          <Footer />
+        <Footer />
       </footer>
     </div>
-  )
+  );
 }
